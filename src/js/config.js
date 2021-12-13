cecb67a4110ead8e503376c0f482ae82fc7cd82f
Robert Miller <miller.robert.john@gmail.com>
2021-12-13 13:03:14 -0700

    Generate new api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const API_KEY = '1c78f3f8-8bd4-466d-9a67-83d97e46b88c';
+export const API_KEY = 'b8a46b02-e7a5-4c7d-917a-28d656ef7f00';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 2.5;
